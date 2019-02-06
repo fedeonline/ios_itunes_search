14fcab40b0b8dab43c6ebbf74b4b9ac7a94192c9
Federico Pugnaloni <federico.pugnaloni@streetlib.com>
2019-02-06 09:16:47 +0100

    Add icons and show keyboard at startup

--- AppStoreSearch/SearchViewController.swift
+++ AppStoreSearch/SearchViewController.swift
@@ -31,6 +31,7 @@ class SearchViewController: UIViewController {
         
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
         tableView.rowHeight = 80
+        searchBar.becomeFirstResponder()
     }
 }
 
